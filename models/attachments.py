30bb9b8865a2871d374d35244bafa6e1f3e81ae3
Richard Austin <richard.e353@gmail.com>
2024-11-01 16:03:20 -0700

    Added processing of optional 3rd and 4th gps files to attachments processing
    Signed-off-by: Richard Austin <richard.e353@gmail.com>

--- models/attachments.py
+++ models/attachments.py
@@ -13,6 +13,8 @@ class DataSetAttachments:
     data_sheet_2: str
     track_gps_file: str
     second_gps_file: str
+    third_gps_file: str
+    fourth_gps_file: str
     spreadsheet_1: str
     spreadsheet_2: str
 
@@ -25,6 +27,10 @@ class DataSetAttachments:
             copy_file_if_exists(file_prefix, src, dest, self.track_gps_file, '_Gps1')
         if len(self.second_gps_file) > 0:
             copy_file_if_exists(file_prefix, src, dest, self.second_gps_file, '_Gps2')
+        if len(self.third_gps_file) > 0:
+            copy_file_if_exists(file_prefix, src, dest, self.second_gps_file, '_Gps3')
+        if len(self.fourth_gps_file) > 0:
+            copy_file_if_exists(file_prefix, src, dest, self.second_gps_file, '_Gps4')
         if len(self.spreadsheet_1) > 0:
             copy_file_if_exists(file_prefix, src, dest, self.spreadsheet_1, '_SpreadSheet1')
         if len(self.spreadsheet_2) > 0:
@@ -44,6 +50,8 @@ def extract_data_attachments(survey_row: Series) -> DataSetAttachments:
         data_sheet_2,
         ph.as_string_or_default(survey_row.Track_data_file),
         ph.as_string_or_default(survey_row.Second_data_file),
+        ph.as_string_or_default(survey_row.Third_data_file),
+        ph.as_string_or_default(survey_row.Fourth_data_file),
         ph.as_string_or_default(survey_row.CSV1_data_file),
         ph.as_string_or_default(survey_row.CSV2_data_file)
     )
